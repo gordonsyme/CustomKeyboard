5a1f7615471b491702e5fc8c1fff4a2f6e313ccc
Gordon Syme <gordonsyme@gmail.com>
2016-10-22 21:23:04 +0100

    Custom UK layout for physical Pixel C keyboard
    Main change is square braces and curly braces can be typed using the US
    layout chords

---
+++ app/src/main/java/net/twiceasgood/customkeyboard/CustomKeyboardReceiver.java
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (C) 2016 Gordon Syme
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// Copied and modified from https://android.googlesource.com/device/google/dragon/+/master/DragonKeyboard/src/com/android/dragonkeyboard/DragonKeyboardReceiver.java
+
+package net.twiceasgood.customkeyboard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CustomKeyboardReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Nothing to do at this time.
+    }
+}
